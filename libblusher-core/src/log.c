18df27cde8e52c3064117ef9a7e2ac6aea9df7b3
Yujŏnja <hardboiled65@gmail.com>
2021-12-20 13:32:06 +0900

    Log function automatically add newline to the format.

--- libblusher-core/src/log.c
+++ libblusher-core/src/log.c
@@ -16,30 +16,33 @@ void bl_log_c(bl_log_level level, const char *format, ...)
 
     if (level == BL_LOG_LEVEL_INFO) {
         char *new_format = malloc(
-            (sizeof(char) * strlen(info)) + (sizeof(char) * strlen(format)) + 1
+            (sizeof(char) * strlen(info)) + (sizeof(char) * strlen(format)) + 2
         );
         strcpy(new_format, info);
         strcpy(new_format + (strlen(info)), format);
-        new_format[strlen(info) + strlen(format)] = '\0';
+        new_format[strlen(info) + strlen(format)] = '\n';
+        new_format[strlen(info) + strlen(format) + 1] = '\0';
         vfprintf(stdout, new_format, argp);
         fflush(stdout);
         free(new_format);
     } else if (level == BL_LOG_LEVEL_WARN) {
         char *new_format = malloc(
-            (sizeof(char) * strlen(warn)) + (sizeof(char) * strlen(format)) + 1
+            (sizeof(char) * strlen(warn)) + (sizeof(char) * strlen(format)) + 2
         );
         strcpy(new_format, warn);
         strcpy(new_format + (strlen(warn)), format);
-        new_format[strlen(warn) + strlen(format)] = '\0';
+        new_format[strlen(warn) + strlen(format)] = '\n';
+        new_format[strlen(warn) + strlen(format) + 1] = '\0';
         vfprintf(stderr, new_format, argp);
         free(new_format);
     } else if (level == BL_LOG_LEVEL_ERROR) {
         char *new_format = malloc(
-            (sizeof(char) * strlen(error)) + (sizeof(char) * strlen(format)) + 1
+            (sizeof(char) * strlen(error)) + (sizeof(char) * strlen(format)) + 2
         );
         strcpy(new_format, error);
         strcpy(new_format + (strlen(error)), format);
-        new_format[strlen(error) + strlen(format)] = '\0';
+        new_format[strlen(error) + strlen(format)] = '\n';
+        new_format[strlen(error) + strlen(format) + 1] = '\0';
         vfprintf(stderr, new_format, argp);
         free(new_format);
     }
