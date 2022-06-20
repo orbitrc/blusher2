a85791aec8907ae21c53f857fe995eb677287432
Yujŏnja <hardboiled65@gmail.com>
2022-06-21 07:27:32 +0900

    SVG class definitions.

--- src/svg.cpp
+++ src/svg.cpp
@@ -1,6 +1,11 @@
 #include <blusher/svg.h>
 
+// Blusher SVG
 #include <blusher/svg/svg-core.h>
+
+// Blusher
+#include <blusher/application.h>
+#include <blusher/resource.h>
 
 namespace bl {
 
@@ -11,10 +16,30 @@ Svg::Svg(const pr::String& path)
             path.starts_with("brc:"_S)) {
         // Path is Blusher resource path.
         // TODO: Implementation.
+        const char *data_path = path.c_str() + 4;
+        const Resource::Data *data = app->resource()->data(data_path);
+        if (data == nullptr) {
+            //
+            return;
+        }
+        this->_core = bl_svg_core_from_data(data->data(), data->size());
     } else {
         // Path is filesystem path.
         // TODO: Implementation.
     }
+}
+
+Image Svg::to_image(uint64_t width, uint64_t height) const
+{
+    uint32_t size;
+    const unsigned char *png = bl_svg_core_png_data(this->_core,
+        width, height, &size);
+
+    if (png == nullptr) {
+        //
+    }
+
+    return Image(png, size);
 }
 
 } // namespace bl
