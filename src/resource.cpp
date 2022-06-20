eebb10b25dd9fc72c53c0a003bbd58fd499185d2
Yujŏnja <hardboiled65@gmail.com>
2022-06-20 18:04:45 +0900

    Resource methods implementation.

--- src/resource.cpp
+++ src/resource.cpp
@@ -1,5 +1,7 @@
 #include <blusher/resource.h>
 
+#include <string.h>
+
 namespace bl {
 
 //===================
@@ -18,6 +20,27 @@ Resource::Data::Data(const char *path, const unsigned char *data, uint64_t size)
 
 Resource::Resource()
 {
+}
+
+void Resource::add_data(Data *data)
+{
+    this->_data.push_back(data);
+}
+
+const Resource::Data* Resource::data(const char *path) const
+{
+    Resource::Data *data = nullptr;
+
+    auto end = this->_data.end();
+    for (auto it = this->_data.begin(); it != end; ++it) {
+        Resource::Data *d = *it;
+        if (strcmp(d->path(), path) == 0) {
+            data = d;
+            break;
+        }
+    }
+
+    return data;
 }
 
 } // namespace bl
