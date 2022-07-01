5867c952e3b54dc8c9466d9ee42afef1d47cdc11
Yujŏnja <hardboiled65@gmail.com>
2022-07-01 13:18:40 +0900

    Event queue simple implementation.

--- include/blusher/event-dispatcher.h
+++ include/blusher/event-dispatcher.h
@@ -1,21 +1,44 @@
 #ifndef _BL_EVENT_DISPATCHER_H
 #define _BL_EVENT_DISPATCHER_H
 
+// C++
+#include <tuple>
+#include <memory>
+
+// Primer
+#include <primer/vector.h>
+
 namespace bl {
 
 class Application;
 
+class View;
+
 class EventDispatcher
 {
 public:
+    template <typename E>
     class EventQueue
     {
     public:
+        using TupleType = std::tuple<View*, std::shared_ptr<E>>;
+
+    public:
+        EventQueue<E>();
+
+        void enqueue(TupleType tuple);
+
+        TupleType dequeue();
+
+        uint64_t length() const;
+
     private:
+        pr::Vector<TupleType> _vec;
     };
 
 public:
     EventDispatcher(Application *application);
+    ~EventDispatcher();
 
 private:
     Application *_application;
--- src/event-dispatcher.cpp
+++ src/event-dispatcher.cpp
@@ -1,12 +1,52 @@
 #include <blusher/event-dispatcher.h>
 
 #include <blusher/application.h>
+#include <blusher/pointer-event.h>
 
 namespace bl {
 
+//================
+// Event Queue
+//================
+template <typename E>
+using EventQueue = EventDispatcher::EventQueue<E>;
+
+template<>
+EventQueue<PointerEvent>::EventQueue()
+{
+}
+
+template<>
+void EventQueue<PointerEvent>::enqueue(EventQueue::TupleType tuple)
+{
+    this->_vec.push(tuple);
+}
+
+template<>
+EventQueue<PointerEvent>::TupleType EventQueue<PointerEvent>::dequeue()
+{
+    auto ret = this->_vec.remove(0);
+
+    return ret;
+}
+
+template<>
+uint64_t EventQueue<PointerEvent>::length() const
+{
+    return this->_vec.length();
+}
+
+//====================
+// Event Dispatcher
+//====================
+
 EventDispatcher::EventDispatcher(Application *application)
 {
-    (void)application;
+    this->_application = application;
+}
+
+EventDispatcher::~EventDispatcher()
+{
 }
 
 } // namespace bl
